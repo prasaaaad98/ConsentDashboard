660f982918b68a8bb4a1ba076602b5d2fb3eb801
Prasad Jore <prasadjore98@gmail.com>
2025-07-18 15:13:49 +0530

    readme

--- components/LanguageClientProvider.tsx
+++ components/LanguageClientProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { LanguageProvider } from "../hooks/useLanguage";
 import React from "react";
-
+ 
 export default function LanguageClientProvider({ children }: { children: React.ReactNode }) {
   return <LanguageProvider>{children}</LanguageProvider>;
 } 
